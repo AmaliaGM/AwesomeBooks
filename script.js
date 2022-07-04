f8da9f3da5cc6bf0b2ef645eb9b555b63b244412
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-07-04 04:27:45 -0300

    html, local storage

---
+++ script.js
@@ -0,0 +1,11 @@
+let library = [
+    {
+        Title: "Chicken soup for the soul",
+        Author: "Jack Canfield",
+    },
+    {
+        Title: "Green eggs and ham",
+        Author: "Dr. Seuss",
+    }
+]
+localStorage.setItem("library", JSON.stringify(books));
