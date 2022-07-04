88c17515bd9d3e828ba6b58a7609dd8401c9e51c
Tiago Marin <tiago.lelinski@gmail.com>
2022-07-04 15:37:17 -0300

    fix storage from add button function

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
- 
-/* /* function addBook() {
-    let lybraryArr = JSON.parse(localStorage.getItem('library'));
-    // if (lybraryArr == null) {
-    //     lybraryArr = [];
-    // }
+// ADD NEW BOOK
+let add = document.querySelector('#add');
+add.addEventListener('click', () => {
+    let libraryArr = JSON.parse(localStorage.getItem('libraryArr'));
+    if (libraryArr == null) {
+        libraryArr = [];
+    }
     let title = document.querySelector("#title").value
     let author = document.querySelector("#author").value
     let newBook = {
         "Title": title,
         "Author": author
     };
-    lybraryArr.push(newBook);
-    localStorage.setItem("lybraryArr", JSON.stringify(lybraryArr)); 
-} 
-
-/* let add = document.querySelector('#add');
-add.addEventListener('click', addBook); */
+    libraryArr.push(newBook);
+    localStorage.setItem("libraryArr", JSON.stringify(libraryArr));
+});
 
 
-/*  let library = []
-let title = document.querySelector("#title").value
-    let author = document.querySelector("#author").value
-let add = document.querySelector('#add');
-add.addEventListener('click', (e) => {
-    let library = JSON.parse(localStorage.getItem('library'));
-    let newItem = {
-       'Title': e.target.dataset.title,
-        'Author': e.target.dataset.author,
-    };
-    library.push(newItem);
-    localStorage.setItem('library', JSON.stringify(library));
-}); 
- */
-
+// SHOW BOOKS ON THE PAGE
