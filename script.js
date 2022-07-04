10f2b5ec8c77bdae247c3b7bc3c19b4fefd6f629
Tiago Marin <tiago.lelinski@gmail.com>
2022-07-04 18:43:36 -0300

    attempt to do remove book funcion

--- script.js
+++ script.js
@@ -22,42 +22,38 @@ function printBooks() {
     let bookSection = document.getElementById('book-section');
     let libraryArr = JSON.parse(localStorage.getItem('libraryArr'));
     for (let i = 0; i < libraryArr.length; i++) {
-        const book = `<div id="book${i+1}"class='book'>
+        const book = `<div id="book${i}"class='book'>
                 <p class='title'>${libraryArr[i].Title}</p>
                 <p class='author'>${libraryArr[i].Author}</p>
-                <button id="remove-book${i+1}" class='remove-btn'>Remove</button>
+                <button id="remove-book${i}" class='remove-btn'>Remove</button>
                 </div>`
         bookSection.innerHTML += book;
     }
 }
 printBooks()
 
-let book2 = document.getElementById('remove-book2');
-book2.addEventListener('click', () => {
-  let libraryArr = JSON.parse(localStorage.getItem('libraryArr'));
-  localStorage.removeItem(libraryArr[1]);
+// REMOVE BOOK
+let book2 = document.getElementById('remove-book1');
+let id = book2.id;
+let i = id.charAt(id.length - 1);
+
+let removeButton = document.querySelectorAll('.remove-btn')
+window.addEventListener('click', (e) => {
+    if (e === button) { }
 });
 
 
+book2.addEventListener('click', () => {
+    let libraryArr = JSON.parse(localStorage.getItem("libraryArr"));
+    localStorage.setItem("index-other", i);
+    // const index = libraryArr.findIndex(item => item.Title === "");
+    // localStorage.setItem("index", index);
 
 
 
-/*
-function printBooks() {
-    let books = document.getElementById('book-section');
-    let booklist = '<div class = "book-list">';
-    let libraryArr = JSON.parse(localStorage.getItem('libraryArr'));
-    for (let book = 0; book < libraryArr.length; book++) {
-        const newBook = `<div class='book'>
-                <p class='title'>${libraryArr[book].title}</p>
-                <p class='author'>${libraryArr[book].author}</p>
-                <button class='remove'>Remove</button>
-                </div>`
-        booklist += book;
-    }
-    booklist += '</div>';
-    books.innerHTML = booklist;
-}
-printBooks();
-*/
+
+    // newlibraryArr = newlibraryArr.filter(value, index, arr) {
+    //     return index != i;
+    // };
+});
 
