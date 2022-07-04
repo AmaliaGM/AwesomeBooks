1ba59db272a36fca00c7cb1675ff01f9ebb9cfe5
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-07-04 08:40:40 -0300

    display booklist

--- script.js
+++ script.js
@@ -13,7 +13,29 @@ add.addEventListener('click', () => {
     };
     libraryArr.push(newBook);
     localStorage.setItem("libraryArr", JSON.stringify(libraryArr));
+    let booklist = document.querySelector('#book-list');
+    
 });
 
+function printBooks() {
+    let books = document.getElementById('book-section');
+    let booklist = '<div class = "book-list">';
+    let libraryArr = JSON.parse(localStorage.getItem('libraryArr'));
+    for (let book =  0;
+        book < libraryArr.length;
+        book += booklist
+        ) {
+            const book = `<div class='book'>
+                <p class='title'>${libraryArr[book].title}</p>
+                <p class='author'>${libraryArr[book].author}</p>
+                <button class='remove'>Remove</button>
+                </div>`
+            booklist += book;
+        }
+            booklist += '</div>';
+           books.innerHTML = booklist;
+}
+printBooks();
 
 // SHOW BOOKS ON THE PAGE
+ 
