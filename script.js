0bd8a0d80434ab6c0339cb02d8ebfed32d615440
Tiago Marin <tiago.lelinski@gmail.com>
2022-07-04 20:03:03 -0300

    fix remove button functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // ADD NEW BOOK
 let add = document.querySelector('#add');
-add.addEventListener('click', () => {
+add.addEventListener('click', (event) => {
+    // event.preventDefault();
     let libraryArr = JSON.parse(localStorage.getItem('libraryArr'));
     if (libraryArr == null) {
         libraryArr = [];
@@ -8,8 +9,9 @@ add.addEventListener('click', () => {
     let title = document.querySelector("#title").value
     let author = document.querySelector("#author").value
     let newBook = {
-        "Title": title,
-        "Author": author
+        ID: Date.now(),
+        Title: title,
+        Author: author
     };
     libraryArr.push(newBook);
     localStorage.setItem("libraryArr", JSON.stringify(libraryArr));
@@ -21,11 +23,12 @@ add.addEventListener('click', () => {
 function printBooks() {
     let bookSection = document.getElementById('book-section');
     let libraryArr = JSON.parse(localStorage.getItem('libraryArr'));
+    //try to use map
     for (let i = 0; i < libraryArr.length; i++) {
         const book = `<div id="book${i}"class='book'>
                 <p class='title'>${libraryArr[i].Title}</p>
                 <p class='author'>${libraryArr[i].Author}</p>
-                <button id="remove-book${i}" class='remove-btn'>Remove</button>
+                <button id="${libraryArr[i].ID}" class='remove-btn'>Remove</button>
                 </div>`
         bookSection.innerHTML += book;
     }
@@ -33,19 +36,39 @@ function printBooks() {
 printBooks()
 
 // REMOVE BOOK
+let removeButton = Array.from(document.querySelectorAll('.remove-btn'));
+removeButton.forEach(function (remove) {
+    remove.addEventListener('click', (event) => {
+        let index = event.target.id
+        let libraryArr = JSON.parse(localStorage.getItem("libraryArr"));
+        libraryArr = libraryArr.filter((book) => {
+            return book.ID != index
+        })
+        localStorage.setItem("libraryArr", JSON.stringify(libraryArr));
+        printBooks()
+    });
+});
+
+/*
+// REMOVE BOOK
 let book2 = document.getElementById('remove-book1');
 let id = book2.id;
 let i = id.charAt(id.length - 1);
 
-let removeButton = document.querySelectorAll('.remove-btn')
-window.addEventListener('click', (e) => {
-    if (e === button) { }
-});
+let removeButton = Array.from(document.querySelectorAll('.remove-btn'));
+removeButton.forEach(function(remove){
+    remove.addEventListener('click', () => {
+        // let libraryArr = JSON.parse(localStorage.getItem("libraryArr"));
+        // newlibraryArr = newlibraryArr.filter(value, index, arr) {
+        //         return index != i;
+        //     };
+    });
+})
+*/
 
-
-book2.addEventListener('click', () => {
-    let libraryArr = JSON.parse(localStorage.getItem("libraryArr"));
-    localStorage.setItem("index-other", i);
+// book2.addEventListener('click', () => {
+//     let libraryArr = JSON.parse(localStorage.getItem("libraryArr"));
+//     localStorage.setItem("index-other", i);
     // const index = libraryArr.findIndex(item => item.Title === "");
     // localStorage.setItem("index", index);
 
@@ -55,5 +78,5 @@ book2.addEventListener('click', () => {
     // newlibraryArr = newlibraryArr.filter(value, index, arr) {
     //     return index != i;
     // };
-});
+// });
 
