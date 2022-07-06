9d2bafd76d6463d1abf0041a8457dd9628f144ac
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-07-06 04:02:20 -0300

    class active-inactive pageDisplay

---
+++ pageDisplay.js
@@ -0,0 +1,25 @@
+let list = document.querySelector('#list');
+let addNew = document.querySelector('#navAdd');
+let contact = document.querySelector('#navContact');
+let addBook = document.querySelector('#add-book');
+let contactForm = document.querySelector('#contact');
+let bookSection = document.querySelector('#book-section')
+console.log(list,addNew,contact,addBook,contactForm);
+
+list.addEventListener('click', () => {
+    addBook.classList.remove('active');
+    contactForm.classList.remove('active');
+    bookSection.classList.remove('inactive')
+})
+
+addNew.addEventListener('click', () => {
+    addBook.classList.add('active'); 
+    bookSection.classList.add('inactive');
+    contactForm.classList.remove('active');
+});
+
+contact.addEventListener('click', () => {
+    contactForm.classList.add('active');    
+    bookSection.classList.add('inactive');
+    addBook.classList.remove('active');
+});
